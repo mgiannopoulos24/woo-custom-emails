dde122e544d1b4d0fccf8038e8b7ec0e4169401e
mgiannopoulos24 <79588074+mgiannopoulos24@users.noreply.github.com>
2026-07-09 15:01:06 +0300

    Update woo-custom-emails.php
    Co-authored-by: Mehul Kaklotar <mehul.kaklotar@gmail.com>

--- woo-custom-emails.php
+++ woo-custom-emails.php
@@ -8,7 +8,7 @@
  * Author: wp3sixty
  * Author URI: http://wp3sixty.com
  * Requires at least: 6.0
- * Tested up to: 6.9
+ * Tested up to: 7.0
  * Requires PHP: 8.2
  * Requires Plugins: woocommerce
  * WC requires at least: 8.0
